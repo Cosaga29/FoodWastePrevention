61ec733a24640318d7dc383ef5b9db15001fa3eb
Martin Edmunds <edmundsm@flip2.engr.oregonstate.edu>
2019-12-01 13:02:33 -0800

    added login route to main

--- server/main.js
+++ server/main.js
@@ -6,7 +6,8 @@
 var express = require('express');
 var mysql = require('./dbcon.js');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
+var path = require('path');
 var app = express();
 var handlebars = require('express-handlebars').create({
         defaultLayout:'main',
@@ -18,10 +19,56 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
-app.use('/people_certs', require('./people_certs.js'));
-app.use('/people', require('./people.js'));
-app.use('/planets', require('./planets.js'));
-app.use('/', express.static('public'));
+
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+
+
+
+
+app.get('/', function(request, response){
+	response.sendFile(path.join(__dirname + '/login.html'));
+
+});
+
+app.post('/auth', function(request, response){
+	var username = request.body.username;
+	var password = request.body.password;
+
+	if(username && password){
+		mysql.pool.query('SELECT * FROM UserTable WHERE username = ? AND password = ?'		 , [username, password], function(error,results,fields){
+			if(results.length > 0){
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			}
+			else {
+				response.send('No user found with that combination!');
+			}
+			response.end();
+			});
+	}
+	else{
+		response.send('Enter username and password.');
+		response.end();
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
 
 app.use(function(req,res){
   res.status(404);
